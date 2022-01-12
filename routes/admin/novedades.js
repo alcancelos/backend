6f34087452508ebc982688cfcb94488a46d46726
Alejandro Martín Cancelos <alcancelos@gmail.com>
2022-01-12 15:41:28 -0300

    Insertar Novedades

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,13 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('./../../models/novedadesModel');
 
-router.get('/', function (req, res, next) {
+var hbs = require('hbs');
+hbs.registerHelper('dateFormat', require('handlebars-dateformat'));
+
+
+router.get('/', async function (req, res, next) {
+    var novedades = await novedadesModel.getNovedades();
     res.render('admin/novedades', {
         layout: 'admin/layout',
         nombre: req.session.nombre,
         apellido: req.session.apellido,
-        isNovedades: true
+        isNovedades: true,
+        novedades
     });
+});
+
+router.get('/agregar', (req, res, next) => {
+    res.render('admin/agregar', {
+        layout: 'admin/layout'
+    });
+});
+
+router.post('/agregar', async (req, res, next) => {
+    try {
+        if (req.titulo != "" && req.body.fecha != "" && req.body.contenido != "") {
+            await novedadesModel.insertNovedad(req.body);
+            res.redirect('/admin/novedades')
+        } else {
+            res.render('admin/agregar', {
+                layout: 'admin/layout',
+                error: true, message: 'Todos los campos son requeridos'
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        res.render('admin/agregar', {
+            layout: 'admin/layout',
+            error: true, message: 'No se carga la novedad'
+        })
+    }
 });
 
 module.exports = router;
