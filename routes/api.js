4811a9f03eec9d4f3ef3788e58c908be13937824
Alejandro Cancelos <alcancelos@gmail.com>
2022-02-11 18:46:57 -0300

    api

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,32 @@
 const app = require('../app');
-
-
-
 var express = require('express');
 var router = express.Router();
 var novedadesModel=require('./../models/novedadesModel');
+var cloudinary = require('cloudinary').v2;
+var hbs = require('hbs');
+hbs.registerHelper('dateFormat', require('handlebars-dateformat'));
+
 
 router.get('/novedades', async function(req,res,next){
     let novedades=await novedadesModel.getNovedades();
+    novedades= novedades.map(novedad=>{
+        if(novedad.img_id){
+            const imagen=cloudinary.url(novedad.img_id,{
+                width:100,
+                height:100,
+                crop:'fill'
+            });
+            return {
+                ...novedad,
+                imagen
+            }
+        }else{
+            return{
+                ...novedad,
+                imagen:''
+            }
+        }
+    });
     res.json(novedades);
 });
 
