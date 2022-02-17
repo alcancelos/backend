80e62ec3e6a66682ec603e96f28d771132ecf04e
Alejandro Cancelos <alcancelos@gmail.com>
2022-02-16 22:00:01 -0300

    Envio de mail

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,7 @@ var novedadesModel=require('./../models/novedadesModel');
 var cloudinary = require('cloudinary').v2;
 var hbs = require('hbs');
 hbs.registerHelper('dateFormat', require('handlebars-dateformat'));
-
+var nodemailer = require('nodemailer')
 
 router.get('/novedades', async function(req,res,next){
     let novedades=await novedadesModel.getNovedades();
@@ -28,6 +28,35 @@ router.get('/novedades', async function(req,res,next){
         }
     });
     res.json(novedades);
+});
+
+router.post('/contacto', async (req,res)=>{
+    const mail={
+        from: `${req.body.email}`,
+        to: 'info@opjuridicocontable.com',
+        subject: 'Contacto Web',
+        html: `<h1>Nombre: ${req.body.name}</h1> 
+        <h2>Email: ${req.body.email}</h2> 
+        <h2>Motivo: ${req.body.subject}</h2>
+        <h3>Mensaje:</h3> 
+        <p>${req.body.message}</p>`
+    }
+
+    const transport = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
+    });
+
+    await transport.sendMail(mail)
+
+    res.status(201).json({
+        error: false,
+        message:'Mensaje enviado'
+    });
 });
 
 module.exports=router;
